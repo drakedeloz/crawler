5e2663a9654a9b38d374f8cce8d3cb8e8c1f1dd4
Drake Delozier <drakedeloz@gmail.com>
2025-02-19 23:58:00 -0600

    getHTML

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseURL := args[0]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
+	fmt.Println(getHTML(baseURL))
+}
+
+func getHTML(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode > 399 {
+		return "", errors.New("failed to get html")
+	}
+	if !strings.Contains(resp.Header.Get("Content-Type"), "text/html") {
+		return "", errors.New("invalid content type")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+
 }
